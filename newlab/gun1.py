c76f28d7be2fb6b80d39903f188423859f4ca955
Vaace <pipyaka3@gmail.com>
2016-11-08 11:02:16 +0300

    2 targets

--- newlab/gun1.py
+++ newlab/gun1.py
@@ -104,16 +104,17 @@ class target():
         self.points += points
         canv.itemconfig(self.id_points, text=self.points)
 
-
 t1 = target()
+t2 = target()
 screen1 = canv.create_text(400, 300, text='', font='28')
 g1 = gun()
 bullet = 0
 balls = []
 
 def new_game(event=''):
-    global gun, t1, screen1, balls, bullet
+    global gun, t1, t2, screen1, balls, bullet
     t1.new_target()
+    t2.new_target()
     bullet = 0
     balls = []
     canv.bind('<Button-1>', g1.fire2_start)
@@ -122,12 +123,18 @@ def new_game(event=''):
 
     z = 0.03
     t1.live = 1
-    while t1.live or balls:
+    t2.live = 1
+    while t1.live and t2.live or balls:
         for b in balls:
             b.move()
-            if b.hittest(t1) and t1.live:
-                t1.live = 0
-                t1.hit()
+            if b.hittest(t1) or b.hittest(t2):
+                if b.hittest(t1):
+                    t1.live=0
+                    t1.hit()
+                else:
+                    t2.live=0
+                    t2.hit()
+            if t1.live==0 and t2.live==0:
                 canv.bind('<Button-1>', '')
                 canv.bind('<ButtonRelease-1>', '')
                 canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
